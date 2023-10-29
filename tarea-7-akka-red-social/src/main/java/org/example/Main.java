7fa7ed71d5aad7e0b85c96858fc28524268b1379
FavioSaico <favio.saico@unmsm.edu.pe>
2023-10-28 22:48:32 -0500

    Añadiendo comentarios

--- tarea-7-akka-red-social/src/main/java/org/example/Main.java
+++ tarea-7-akka-red-social/src/main/java/org/example/Main.java
@@ -5,60 +5,67 @@ import akka.actor.ActorRef;
 import akka.actor.Props;
 
 public class Main {
+
+    // creamos una función para colocar un tiempo espera, esto para poder mostrar mejor los resultados
+    // dado que los actores se ejecutan en paralelo, un actor puede imprimir los mensajes antes que otro y mostrar los resultados desordenados
+    public static void esperar(int time){
+        try {
+            Thread.sleep(time);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
     public static void main(String[] args) {
 
         // Crea el sistema de actores
         ActorSystem system = ActorSystem.create("CadenaActores");
 
-        // Crea los actores en la cadena
+        // Crea los actores o usuario para este caso
         ActorRef actorD = system.actorOf(Props.create(User.class, "ActorD"));
         ActorRef actorC = system.actorOf(Props.create(User.class, "ActorC"));
         ActorRef actorB = system.actorOf(Props.create(User.class, "ActorB"));
         ActorRef actorA = system.actorOf(Props.create(User.class, "ActorA"));
 
+
         // ENVIAR MENSAJES
+        // asumimos que los mensajes se pueden enviar desde cualquier actor a otro, no es necesario seguirlo.
+        // esto lo hacemos enviando la clase MensajeCadena
         MensajeCadena ms = new MensajeCadena("Hola como estas");
         actorA.tell(ms,actorB);
+        esperar(1000);
+        System.out.println();
+
 
         // AGREGAR SEGUIDOR
+        // agregamos seguidores a nuestros usuarios a traves de clase Follow
         actorA.tell(new Follow(),actorB); // // actor B envía un mensaje al actor A. B sigue a A
         actorA.tell(new Follow(),actorC); // C sigue a A
 
         actorB.tell(new Follow(),actorD); // D sigue a B
-        actorB.tell(new Follow(),actorC); // D sigue a B
+        actorB.tell(new Follow(),actorC); // C sigue a B
+        esperar(1000);
+        System.out.println();
 
-        // tiempo de espera
-        try {
-            Thread.sleep(1000);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
 
         // OBTENER LA LISTA DE SEGUIDORES
-        // Obtener la lista de seguidores
+        // Obtenemos la lista de seguidore del usuario A, pasando la clase GetListFollow() que definimos dentro de User
         actorA.tell(new User.GetListFollow(),ActorRef.noSender());
+        esperar(1000);
 
-        // tiempo de espera
-        try {
-            Thread.sleep(1000);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
+        // Obtenemos la lista de seguidores del usuario B
         actorB.tell(new User.GetListFollow(),ActorRef.noSender());
+        esperar(1000);
+        System.out.println();
 
-        try {
-            Thread.sleep(1000);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
 
         // PUBLICACIONES
-        // Realizar una publicación
+        // Realizamos una publicación, esta publicación la realiza el usuario A.
         Publication publicacion1 = new Publication("Publicación: Esta es la publicación del Actor A");
         actorA.tell(publicacion1,ActorRef.noSender());
-
-        // Detiene el sistema de actores hasta que mensajes se procesen
+        esperar(1000);
+        System.out.println();
+        // Detiene el sistema de actores hasta que los mensajes se procesen
         system.terminate();
 
     }
--- tarea-7-akka-red-social/src/main/java/org/example/User.java
+++ tarea-7-akka-red-social/src/main/java/org/example/User.java
@@ -6,35 +6,48 @@ import akka.actor.ActorRef;
 import java.util.ArrayList;
 import java.util.List;
 
+// Clase User que extiene de AbstractActor para crear actores en base a esta clase
 public class User extends AbstractActor {
 
+    // Cada actor tendrá un nomnre una lista de seguidores, que son otros actores.
     private final String nombre;
     private List<ActorRef> UserFollow;
+
+    // Cada vez que instanciemos esta clase debemos pasar el nombre del usuario.
+    // Automaticamente se inicializa el arreglo de usuario como un ArrayList
     public User(String nombre) {
         this.nombre = nombre;
         this.UserFollow = new ArrayList<>();
     }
 
+    // Clase estatica para obtener el nombre de lo seguidores del usuario.
     public static class GetListFollow{}
 
-    // acciones que realiza al recibir mensajes
+    // Acciones que realiza al recibir mensajes
     @Override
     public AbstractActor.Receive createReceive() {
         return receiveBuilder()
-                .match(MensajeCadena.class, mensaje -> { // ENVIAR MENSAJES
-                    // Agrega información al mensaje, le añade el nombre del actor emisor y receptor
+                // ENVIAR MENSAJES
+                // Cuando se envia al usuario la clase MensajeCadena, este debe mostrar el mensaje que se envío sumado al nombre del actor emisor
+                // para obtener el nombre del actor emisor del mensaje, debemos enviar un mensaje con la clase ShowMessage al emisor.
+                .match(MensajeCadena.class, mensaje -> {
                     getSender().tell(new ShowMessage(" envio '" + mensaje.getContenido() + "' a " + nombre),ActorRef.noSender());
 
                 })
-                .match(Follow.class, seguir -> { // AGREGAR SEGUIDOR
-                    // UserFollow es del actor receptor
-                    // getSender es el acto que envia el mensaje
-                    UserFollow.add(getSender()); // Añadimos al emisor a la lista de seguidores
+                // AGREGAR SEGUIDOR
+                // Para agregar un nuevo seguidor se debe enviar como mensaje la clase Follow,
+                // Luego añadimos al usuario emisor a la lista del seguidores del usuario receptor
+                // Por ultimo, imprimimos en consola que actor sigue a este actor para confirmar que se agrego.
+                .match(Follow.class, seguir -> {
+                    UserFollow.add(getSender()); // agrega a la lista
 
                     String content = " sigue a " + nombre;
                     getSender().tell(new ShowMessage(content),ActorRef.noSender());
                 })
-                .match(GetListFollow.class, text ->{ // OBTENER LA LISTA DE SEGUIDORES
+                // OBTENER LA LISTA DE SEGUIDORES
+                // Para imprimir por consola la lista de seguidores de un usuario debemos enviar la clase GetListFollow
+                // Iteremos la lista de seguidores y cada uno le enviamos la clase ShowMessage para que pueda mostrar su nombre.
+                .match(GetListFollow.class, text ->{
                     System.out.printf("Lista de seguidores de " + nombre + "\n");
                     System.out.printf("Cantidad de seguidores: " + UserFollow.size() + "\n");
                     // Mostramos el nombre de los seguidores
@@ -43,7 +56,10 @@ public class User extends AbstractActor {
                     );
 
                 })
-                .match(Publication.class, publication-> { // PUBLICACIONES
+                // PUBLICACIONES
+                // Para que usuario pueda realizar publicaciones debe recibir la clase Publication
+                // Imprimimos por consola el contenido de la publicación e iteramos sobre su lista de siguidores en señal de que recibieron la publicación
+                .match(Publication.class, publication-> {
                     System.out.printf(publication.getContenido() + "\n");
                     System.out.printf("Enviando publicación a los seguidores..." + "\n");
 
@@ -51,7 +67,10 @@ public class User extends AbstractActor {
                             actorRef.tell(new ShowMessage(" recibo la publicación de " + nombre), ActorRef.noSender())
                     );
                 })
-                .match(ShowMessage.class, message-> { // IMPRIMIR POR CONSOLA
+                // IMPRIMIR POR CONSOLA
+                // Esta acción recibe la clase ShowMessage para imprimir por consola el nombre y el contenido que venga en la clase
+                // esta acción la usuamos en las anteriores para imprimir los nombres de los actores.
+                .match(ShowMessage.class, message-> {
                         System.out.printf(nombre + message.getContent() + "\n");
 
                 })
