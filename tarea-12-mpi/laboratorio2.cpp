55489c12b93721cfcd4849140764290fc2291928
root <root@MAIN>
2023-11-25 13:10:25 -0500

    lab2 completo

--- tarea-12-mpi/laboratorio2.cpp
+++ tarea-12-mpi/laboratorio2.cpp
@@ -1,26 +1,25 @@
 /*
-
-	Laboratorio 2: Procesamiento de Imágenes en Paralelo
+	Laboratorio 2: Procesamiento de Imagenes en Paralelo
 	Objetivo: Implementar un algoritmo paralelo utilizando MPI para procesar una 
-				imagen de gran tamaño aplicando una serie de filtros.
+				imagen de gran tamanio aplicando una serie de filtros.
 	Pasos:
 	- Dividir la imagen en partes iguales y distribuir cada parte entre los procesos MPI.
-	- Cada proceso realiza el procesamiento de la imagen aplicando un filtro específico a su parte.
-	- Realizar una comunicación punto a punto (por ejemplo, MPI_Send y MPI_Recv) para intercambiar
-		las partes de la imagen entre los procesos vecinos, de manera que puedan procesar los píxeles 
+	- Cada proceso realiza el procesamiento de la imagen aplicando un filtro especifico a su parte.
+	- Realizar una comunicacion punto a punto (por ejemplo, MPI_Send y MPI_Recv) para intercambiar
+		las partes de la imagen entre los procesos vecinos, de manera que puedan procesar los pixeles 
 		de borde correctamente.
-	- Realizar una comunicación colectiva (por ejemplo, MPI_Gather) para combinar las partes procesadas
+	- Realizar una comunicacion colectiva (por ejemplo, MPI_Gather) para combinar las partes procesadas
 		y reconstruir la imagen completa en el proceso maestro.El proceso maestro guarda la 
 		imagen resultante en un archivo y la muestra.
-
 */
 #include <iostream>
 #include <mpi.h>
 #include <opencv2/opencv.hpp>
-
+#include <string>
 using namespace cv;
 
 void convertirBlancoNegro(Mat& img, int inicio, int fin) {
+    // recorido de los pixeles
     for (int i = inicio; i < fin; ++i) {
         for (int j = 0; j < img.cols; ++j) {
             Vec3b pixel = img.at<Vec3b>(i, j);
@@ -37,16 +36,9 @@ int main(int argc, char** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    if (argc != 3) {
-        if (rank == 0) {
-            std::cerr << "Uso: " << argv[0] << " <imagen_entrada.jpg> <imagen_salida.jpg>" << std::endl;
-        }
-        MPI_Finalize();
-        return 1;
-    }
-
     // Solo el proceso 0 lee la imagen
     Mat imagen;
+    int filas, cols;
     if (rank == 0) {
         imagen = imread(argv[1]);
         if (imagen.empty()) {
@@ -54,14 +46,12 @@ int main(int argc, char** argv) {
             MPI_Finalize();
             return 1;
         }
-    }
-
-    // Broadcast del tamaño de la imagen
-    int filas, cols;
-    if (rank == 0) {
+        // asignamos el valor de las filas y columnas
         filas = imagen.rows;
         cols = imagen.cols;
     }
+
+    // Broadcast de las filas y columnas de la imagen para todos los procesos
     MPI_Bcast(&filas, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&cols, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
@@ -74,17 +64,33 @@ int main(int argc, char** argv) {
     if (rank != 0) {
         imagen = Mat(filas, cols, CV_8UC3);
     }
-    MPI_Bcast(imagen.data, filas * cols * 3, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+    
+    // Desde el proceso 0 enviamos la imagen al resto de procesos 
+    if (rank == 0) {
+        for (int i=1; i<size; i++){
+            MPI_Send(imagen.data, filas * cols * 3, MPI_UNSIGNED_CHAR, i, 0, MPI_COMM_WORLD);
+        }
+    }
+    
+    // Los demÃ¡s procesos reciben la imagen desde el el proceso 0
+    if (rank != 0) {
+        MPI_Recv(imagen.data, filas * cols * 3, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
 
-    // Procesamiento paralelo de la imagen
+    // Procesamiento de la imagen
     convertirBlancoNegro(imagen, inicio, fin);
 
-    // Recopila los resultados
+    // generamos una imagen por cada parte en blanco y negro
+    std::string name = std::string(argv[1]) + std::to_string(rank) + ".jpg";
+    imwrite(name, imagen);
+
+    // Recopila los resultados de todos los procesos
     MPI_Gather(imagen.data + inicio * cols * 3, (fin - inicio) * cols * 3, MPI_UNSIGNED_CHAR, imagen.data, (fin - inicio) * cols * 3, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
 
     // Solo el proceso 0 guarda la imagen resultante
     if (rank == 0) {
-        imwrite(argv[2], imagen);
+        name = std::string(argv[1]) + "_BN.jpg";
+        imwrite(name, imagen);
     }
 
     MPI_Finalize();
